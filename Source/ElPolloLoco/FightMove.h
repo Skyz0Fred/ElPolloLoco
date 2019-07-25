51b7ede8d5016707a5c902cdbbfde58053949b0e
Frederic Ouellet <pouet43@hotmail.com>
2019-07-25 13:07:25 -0400

    Fight Move Struct conversion
    Added code to have a struct in the fight move data asset containing the various properties afterwards.
    Removed some unused code.
    I'll be able to start doing the system afterwards.

--- Source/ElPolloLoco/FightMove.h
+++ Source/ElPolloLoco/FightMove.h
@@ -15,7 +15,7 @@
 
  //Types Of Attacks - added later in the data asset to distinguish attacks
 UENUM()
-enum AttackTypes
+enum EAttackTypes
 {
 	GroundAttack UMETA(DisplayName = "Ground Attack"),
 	AirAttack UMETA(DisplayName = "Air Attack"),
@@ -24,51 +24,79 @@ enum AttackTypes
 	FinishingAttack UMETA(DisplayName = "Finishing Attack")
 };
 
+USTRUCT(BlueprintType)
+struct FFightMoveStruct
+{
+	GENERATED_BODY()
 
-UCLASS(BlueprintType, hidecategories=(Object), DisplayName="El Pollo Fight Move")
+	//Name Of Attack
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FString AttackName = "AttackName";
+
+	//Level Of Attack
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int Level = 1;
+
+	//Animation asset to use for move
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UAnimSequence *Animation = nullptr;
+
+	//Damage inflicted by move
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float DamageInflicted = 0.0f;
+
+	//Attack type defined in enum
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TEnumAsByte<EAttackTypes> AttackType;
+
+	//Cooldown Before Next Attack
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float CooldownBeforeNext = 0.0f;
+};
+
+UCLASS(BlueprintType, DisplayName="El Pollo Fight Move")
 //class ELPOLLOLOCO_API UFightMove: public UObject
 class ELPOLLOLOCO_API UFightMove : public UDataAsset
 {
 	GENERATED_BODY()
 
-private:
-		FString someString = "Something";
-		enum AttackType;
-
 public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,meta = (ShowOnlyInnerProperties))
+		FFightMoveStruct FightMove;
+	/*
 	//Name Of Attack
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
 	FString AttackName = "AttackName";
+
+	//Level Of Attack
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
+		int Level = 1;
+
+	//Animation asset to use for move
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
+		UAnimSequence *Animation = nullptr;
+
+	//Damage inflicted by move
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
+		float DamageInflicted = 0.0f;
+
+	//Attack type defined in enum
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
+		TEnumAsByte<EAttackTypes> AttackType;
+
+	//Cooldown Before Next Attack
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
+		float CooldownBeforeNext = 0.0f;
+	*/
 	
 	/*Displays any UClasses driving from UAnimSequence in a dropdown menu in Blueprints
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="FightMove")
 	TSubclassOf<UAnimSequence> Animation; 
 	*/
 
-	/*Level Of Attack
-	*/
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
-	int Level = 1;
-
-	//Animation asset to use for move
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
-	UAnimSequence *Animation = nullptr;
-
-	//Damage inflicted by move
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
-	float DamageInflicted = 0.0f;
-	
-	//Attack type defined in enum
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
-		TEnumAsByte<AttackTypes> AttackType;
-
 	/*Displays any blueprints derived from that code class
 	UPROPERTY(EditAnywhere, meta=(MetaClass = "UserDefinedEnum"), Category = "FightMove") 
 	FStringClassReference Enum;
 	*/
-	
-	//Cooldown Before Next Attack
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FightMove")
-	float CooldownBeforeNext = 0.0f;
-
 };
