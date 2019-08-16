b5df298a20cec05ad3e0e710d4f05d5ab48e71ad
Skyz0Fred <49761581+Skyz0Fred@users.noreply.github.com>
2019-08-16 14:08:53 -0400

    Splash and code
    Added a custom splash screen and a temp icon.
    Added cpp and h of new interface class i'll use later on for new anim BP class.

---
+++ Source/ElPolloLoco/Public/ElPollo_AnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ElPollo_AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELPOLLOLOCO_API UElPollo_AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
