ebb77818bfbf8db579ca7ebde086eefc75400c5d
Skyz0Fred <49761581+Skyz0Fred@users.noreply.github.com>
2019-09-08 14:31:03 -0400

    Cleanup and continued spawnner
    Added random spawn points functionality in spawner.
    Renamed InterfaceAnim to ElPollo_InterfaceAnim.
    Cleanup.
    Changed  mass settings for physic bodies.
    Fixed a problem with character hitting tiggers.

---
+++ Source/ElPolloLoco/Public/ElPollo_InterfaceAnim.h
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ElPollo_InterfaceAnim.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(BlueprintType, MinimalAPI)
+class UElPollo_InterfaceAnim : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ELPOLLOLOCO_API IElPollo_InterfaceAnim
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void Jumping();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void Flinching();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void Grabbing();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void Dropping();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void Throwing();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void AirAttacking();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void Attacking();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void SprintAttacking();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void AttackBegin();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void AttackEnd();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void ComboCancel();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "El Pollo Anim")
+	void ComboLink();
+
+};
